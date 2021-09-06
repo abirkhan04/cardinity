b1b9cae857ee51b6792c1a5f21d71ea31999e22d
abirkhan04 <abirkhan04@users.noreply.github.com>
2021-09-07 05:00:16 +0600

    Update ProjectRepository.java

--- TaskManager/src/main/java/com/cardinity/repositories/ProjectRepository.java
+++ TaskManager/src/main/java/com/cardinity/repositories/ProjectRepository.java
@@ -2,14 +2,11 @@ package com.cardinity.repositories;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.repository.PagingAndSortingRepository;
-
 import com.cardinity.pojo.Project;
 import com.cardinity.pojo.User;
 
 public interface ProjectRepository<T> extends PagingAndSortingRepository<Project, Long> {
 	List<Project> findByUser(User user);
-
 	Optional<Project> findByUserAndId(User user, Long id);
 }
